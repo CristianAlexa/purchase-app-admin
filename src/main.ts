ca70224b799a9a0c34454746c70bc3de4805c9cc
CristianAlexa <cristian.alexa@gmail.com>
2024-08-17 16:59:57 +0100

    firebase auth signup added

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,7 @@
-// document.querySelector<HTMLDivElement>("")!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `;
-
-// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
-
 const signupBtn = document.querySelector<HTMLButtonElement>("#signup-btn");
+const loginBtn = document.querySelector<HTMLButtonElement>("#login-btn");
 const signupBtnLogin =
   document.querySelector<HTMLButtonElement>("#signup-btn-login");
-const loginBtn = document.querySelector<HTMLButtonElement>("#login-btn");
 const cancelSignupBtn =
   document.querySelector<HTMLButtonElement>("#cancel-signup-btn");
 const cancelLoginBtn =
@@ -37,7 +17,10 @@ loginBtn?.addEventListener("click", () => {
   loginModal?.classList.add("active");
 });
 
-cancelSignupBtn?.addEventListener("click", (e) => {
+cancelSignupBtn?.addEventListener("click", () => {
+  const signupForm = document.querySelector<HTMLFormElement>("#signup-form");
+  signupForm["signup-email"].value = "";
+  signupForm["signup-password"].value = "";
   signupModal?.classList.remove("active");
 });
 
